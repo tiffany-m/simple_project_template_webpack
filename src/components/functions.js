a27b91ca45fc53285a9b88ace9b9d503062f5f30
Tiffany <67019104+tiffany-m@users.noreply.github.co>
2024-01-22 11:36:40 -0500

    Updated functions file

--- src/components/functions.js
+++
@@ -1,7 +0,0 @@
-// Test Module -----------------------------------------------------------------------
-export function changeButton() {
-  const btn = document.getElementById("test-button");
-
-  btn.addEventListener("click", () => btn.classList.add("function-test"));
-}
-// -----------------------------------------------------------------------------
